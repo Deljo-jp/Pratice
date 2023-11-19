1c03195de9bb94e146052ebb8f5325037500b3ab
deljo jp <deljojpalathingal@gmail.com>
2023-11-19 11:56:30 +0530

    nov 19 first commit

---
+++ JavaScript/Operations.js
@@ -0,0 +1,45 @@
+// Operator
+
+//Assignment Operator
+let x = 10;
+let y = 10;
+
+//Arithematic Operator
+// + , - , *  , / , % 
+let z = x + y;
+console.log(z);
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+
+//Comparision Operators
+
+let abc = 10;
+let xyz = '10';
+console.log(abc > xyz);
+console.log(abc < xyz);
+console.log(abc >= xyz);
+
+// == and ===
+
+// == (compairs values but not datatype)
+console.log(abc == xyz);
+
+// === (compairs values and datatypes)
+
+console.log(abc === xyz);
+
+//Conditions or Decisions
+
+let age = 21;
+if (age >= 18) {
+console.log('You are allowed to apply for license');
+}
+else {
+console.log('Not allowed');
+}
+
+
---
+++ JavaScript/Values.js
@@ -0,0 +1,46 @@
+                //Primitive Values => Only Single value
+// Numbers Values
+7;
+5.5;
+
+// String Values
+("Deljo")
+('Lijo')
+
+//Boolean Values
+true = 0;
+false = 1;
+
+//Empty Values
+null;
+undefined;
+                //Non-Primitive Values => Will more than one values
+
+//Array and Object
+
+Array = [1,2,3,4,true,'Deljo']
+         0,1,2,3, 4  ,   5 == Index
+
+{
+    key:value
+    fname : Deljo,
+    lname = Jp,
+}
+
+          // Variables = place holder for values
+
+var y = 25;
+let x = 50;   // Values get changed, EG: Bank Balance
+const z = '7373957835568'; // Constant = Value does not get changed, EG: Bank account number
+
+let fname = 'Deljo';
+let lname = 'J palathingal';
+const mobilenumber = 78959347454;
+
+console.log('My first name is:',fname);
+console.log('My last name is:'+ lname);
+console.log('My Number is:',mobilenumber);
+
+// Template literal
+
+console.log(`My current first name is ${fname} ${lname} ${mobilenumber}`);
