93dc9f1b9f4551fb456d117e9fedba4aadc5de62
deljo jp <deljojpalathingal@gmail.com>
2024-07-16 19:40:06 +0530

    July14

--- Namesthe javascrpt/let.js
+++ Namesthe javascrpt/let.js
@@ -1,4 +1,44 @@
+
+let a = 19;
 console.log(a); 
+
+// let a = 20; Cannot modify let
+
+var b = 199;
 console.log(b);
-let a = 10;
-var b = 100;
+
+var b = 26;
+console.log(b);
+
+const c = 25; 
+console.log(c);
+// const c = 20; GIves a type error
+
+
+// Block
+
+if(true) {
+    // Compound statement
+    var d = 76;
+    console.log(d);
+}
+
+
+// Block Scope
+var x = 100;
+let y = 200;
+const z = 300;
+
+{
+    var x = 10;
+    let y = 20;
+    const z = 30;
+    console.log(x);
+    console.log(y);
+    console.log(z);
+
+}
+// Cannot access let and const in block
+console.log(x);
+console.log(y);
+console.log(z);  
